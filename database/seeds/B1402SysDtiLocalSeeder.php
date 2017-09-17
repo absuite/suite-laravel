7cc8046fcde976982948f281eb5e47ce50edfc63
ggoop <samuel_abe@163.com>
2017-09-17 21:27:55 +0800

    init

--- database/seeds/B1402SysDtiLocalSeeder.php
+++ database/seeds/B1402SysDtiLocalSeeder.php
@@ -14,67 +14,67 @@ class B1402SysDtiLocalSeeder extends Seeder {
 		Models\DtiLocal::where('id', '!=', '')->delete();
 
 		Models\DtiLocal::build(function (Builder $b) {
-			$b->method_enum('post')->code("P_ISV_TRANSDATA_CBO_ORG")->name("组织")->path('api/cbo/orgs/batch');
+			$b->method_enum('post')->code("api/cbo/orgs/batch")->name("组织")->path('api/cbo/orgs/batch');
 		});
 
 		Models\DtiLocal::build(function (Builder $b) {
-			$b->method_enum('post')->code("P_ISV_TRANSDATA_CBO_CURRENCY")->name("币种")->path('api/cbo/currencies/batch');
+			$b->method_enum('post')->code("api/cbo/currencies/batch")->name("币种")->path('api/cbo/currencies/batch');
 		});
 
 		Models\DtiLocal::build(function (Builder $b) {
-			$b->method_enum('post')->code("P_ISV_TRANSDATA_CBO_UOM")->name("计量单位")->path('api/cbo/units/batch');
+			$b->method_enum('post')->code("api/cbo/units/batch")->name("计量单位")->path('api/cbo/units/batch');
 		});
 
 		Models\DtiLocal::build(function (Builder $b) {
-			$b->method_enum('post')->code("P_ISV_TRANSDATA_CBO_DEPT")->name("部门")->path('api/cbo/depts/batch');
+			$b->method_enum('post')->code("api/cbo/depts/batch")->name("部门")->path('api/cbo/depts/batch');
 		});
 
 		Models\DtiLocal::build(function (Builder $b) {
-			$b->method_enum('post')->code("P_ISV_TRANSDATA_CBO_WORK")->name("工作中心")->path('api/cbo/works/batch');
+			$b->method_enum('post')->code("api/cbo/works/batch")->name("工作中心")->path('api/cbo/works/batch');
 		});
 
 		Models\DtiLocal::build(function (Builder $b) {
-			$b->method_enum('post')->code("P_ISV_TRANSDATA_CBO_WH")->name("存储地点")->path('api/cbo/whs/batch');
+			$b->method_enum('post')->code("api/cbo/whs/batch")->name("存储地点")->path('api/cbo/whs/batch');
 		});
 
 		Models\DtiLocal::build(function (Builder $b) {
-			$b->method_enum('post')->code("P_ISV_TRANSDATA_CBO_PERSON")->name("人员")->path('api/cbo/persons/batch');
+			$b->method_enum('post')->code("api/cbo/persons/batch")->name("人员")->path('api/cbo/persons/batch');
 		});
 
 		Models\DtiLocal::build(function (Builder $b) {
-			$b->method_enum('post')->code("P_ISV_TRANSDATA_CBO_PROJECT_CATEGORY")->name("项目分类")->path('api/cbo/project-categories/batch');
+			$b->method_enum('post')->code("api/cbo/project-categories/batch")->name("项目分类")->path('api/cbo/project-categories/batch');
 		});
 		Models\DtiLocal::build(function (Builder $b) {
-			$b->method_enum('post')->code("P_ISV_TRANSDATA_CBO_PROJECT")->name("项目")->path('api/cbo/projects/batch');
-		});
-
-		Models\DtiLocal::build(function (Builder $b) {
-			$b->method_enum('post')->code("P_ISV_TRANSDATA_CBO_ITEM_CATEGORY")->name("物料分类")->path('api/cbo/item-categories/batch');
-		});
-		Models\DtiLocal::build(function (Builder $b) {
-			$b->method_enum('post')->code("P_ISV_TRANSDATA_CBO_ITEM")->name("物料")->path('api/cbo/items/batch');
+			$b->method_enum('post')->code("api/cbo/projects/batch")->name("项目")->path('api/cbo/projects/batch');
 		});
 
 		Models\DtiLocal::build(function (Builder $b) {
-			$b->method_enum('post')->code("P_ISV_TRANSDATA_CBO_TRADER_CATEGORY")->name("客商分类")->path('api/cbo/trader-categories/batch');
+			$b->method_enum('post')->code("api/cbo/item-categories/batch")->name("物料分类")->path('api/cbo/item-categories/batch');
 		});
 		Models\DtiLocal::build(function (Builder $b) {
-			$b->method_enum('post')->code("P_ISV_TRANSDATA_CBO_TRADER")->name("客商")->path('api/cbo/traders/batch');
+			$b->method_enum('post')->code("api/cbo/items/batch")->name("物料")->path('api/cbo/items/batch');
 		});
 
 		Models\DtiLocal::build(function (Builder $b) {
-			$b->method_enum('post')->code("P_ISV_TRANSDATA_CBO_DOC_TYPE")->name("单据类型")->path('api/cbo/doc-types/batch');
+			$b->method_enum('post')->code("api/cbo/trader-categories/batch")->name("客商分类")->path('api/cbo/trader-categories/batch');
+		});
+		Models\DtiLocal::build(function (Builder $b) {
+			$b->method_enum('post')->code("api/cbo/traders/batch")->name("客商")->path('api/cbo/traders/batch');
+		});
+
+		Models\DtiLocal::build(function (Builder $b) {
+			$b->method_enum('post')->code("api/cbo/doc-types/batch")->name("单据类型")->path('api/cbo/doc-types/batch');
 		});
 
 		//业务数据
 		Models\DtiLocal::build(function (Builder $b) {
-			$b->method_enum('post')->code("P_ISV_TRANSDATA_TRADE_MISCRCV")->name("业务数据")->path('api/amiba/doc-bizs/batch');
+			$b->method_enum('post')->code("api/amiba/doc-bizs/batch")->name("业务数据")->path('api/amiba/doc-bizs/batch');
 			$b->body('{"FromDate":"${fm_date}","toDate":"${to_date}"}');
 		});
 
 		//财务数据
 		Models\DtiLocal::build(function (Builder $b) {
-			$b->method_enum('post')->code("P_ISV_TRANSDATA_TRADE_GL_VOUCHER")->name("财务数据")->path('api/cbo/doc-fis/batch');
+			$b->method_enum('post')->code("api/cbo/doc-fis/batch")->name("财务数据")->path('api/cbo/doc-fis/batch');
 			$b->body('{"FromDate":"${fm_date}","toDate":"${to_date}"}');
 		});
 	}
