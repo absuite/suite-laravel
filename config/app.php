61efaafbf3804beee0fe60bfbbcdfc4266b71443
pull user <pull user@163.com>
2017-11-14 22:24:21 +0800

    init

--- config/app.php
+++ config/app.php
@@ -245,6 +245,8 @@ return [
 		//QrCode
 		'QrCode' => SimpleSoftwareIO\QrCode\Facades\QrCode::class,
 		'Excel' => Maatwebsite\Excel\Facades\Excel::class,
+
+		'Ent' => Gmf\Sys\EntFacade::class,
 	],
 
 ];
