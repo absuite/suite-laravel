fa4431e0a80909d91331645c63e128c54c76e911
pull user <pull user@163.com>
2018-05-27 17:18:52 +0800

    init

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,38 +1,28 @@
-window.Vue = window.Vue || require('vue');
+import gapp from 'gmf/app';
+import component from 'gmf/component-all';
 
-
-import { start } from './vendor/gmf-sys';
 import cbo from './vendor/suite-cbo';
 import amiba from './vendor/suite-amiba';
 import bec from './vendor/suite-bec';
 import docs from './vendor/suite-docs';
 import http from 'gmf/core/utils/http';
-Vue.use(docs);
-Vue.use(cbo);
-Vue.use(amiba);
-Vue.use(bec);
 
+import routesAuth from 'gmf/routes/auth';
+import routesMd from 'gmf/routes/md';
 
+gapp.use(component);
+gapp.use(docs);
+gapp.use(cbo);
+gapp.use(amiba);
+gapp.use(bec);
+gapp.route(routesAuth);
+gapp.route(routesMd);
 
-const appMixin = {
-  methods: {
-    async loadConfigs() {
-      return await this.$http.get('/site/configs');
-    },
-    async beforeCreated() {
-      this.configs.home = '/app/dashboard';
-      this.configs.loadEnum = true;
-      if (window.gmfConfig) {
-        this.$setConfigs(window.gmfConfig);
-      }
-    }
-  }
-};
-
-var promise = new Promise(function(resolve, reject) {
-  http.get('/site/configs').then(res => {
-    window.gmfConfig = res.data.data;
-    resolve();
+gapp.config(function() {
+  return new Promise(function(resolve, reject) {
+    http.get('/site/configs').then(res => {
+      resolve(res.data.data);
+    });
   });
 });
-start.run({promise}, appMixin);
+gapp.run({});
